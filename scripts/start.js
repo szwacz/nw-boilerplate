f5ac0d17e1c8f2fa4b1b0ad12f847e65bed44545
Kuba <kuba@Kubas-Mac.local>
2014-08-12 10:08:23 -0700

    fixed app start

--- scripts/start.js
+++ scripts/start.js
@@ -7,19 +7,23 @@ var utils = require('./internal/utils');
 
 // First start the gulp watch
 
-var watch = childProcess.spawn('node', ['./node_modules/gulp/bin/gulp', 'watch', '--color']);
+var watch = childProcess.spawn('node', ['./node_modules/.bin/gulp', 'watch', '--color']);
 watch.stdout.pipe(process.stdout);
 watch.stderr.pipe(process.stderr);
 
 // Then start the app
-
-var commands = {
-    'osx': 'open ./build/node-webkit.app',
-    'linux': './build/nw',
-    'windows': './build/nw.exe'
-};
-
-var app = childProcess.spawn(commands[utils.os()]);
+var app;
+switch (utils.os()) {
+    case 'osx':
+        app = childProcess.spawn('open', ['./build/node-webkit.app']);
+        break;
+    case 'linux':
+        app = childProcess.spawn('./build/nw');
+        break;
+    case 'windows':
+        app = childProcess.spawn('./build/nw.exe');
+        break
+}
 app.stdout.pipe(process.stdout);
 app.stderr.pipe(process.stderr);
 app.on('close', function (code) {
