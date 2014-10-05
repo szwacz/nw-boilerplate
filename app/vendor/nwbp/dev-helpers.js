1c768bb753a2b0d2e470b3c77bb1fa6f789094e3
Jakub Szwacz <jakub@szwacz.com>
2014-10-05 15:00:24 +0200

    Fixed keyboard shortcuts for mac

--- app/vendor/nwbp/dev-helpers.js
+++ app/vendor/nwbp/dev-helpers.js
@@ -7,16 +7,29 @@
     var gui = require('nw.gui');
     
     var registerShortcuts = function () {
+        var cmdPressed = false;
+        
+        var keyDown = function () {
+            if (e.keyCode == 91 || e.keyCode == 93) {
+                cmdPressed = true;
+            }
+        };
+        
         var keyUp = function (e) {
-            if ((e.ctrlKey || e.metaKey) && e.keyCode == 82) {
+            if (e.keyCode == 91 || e.keyCode == 93) {
+                cmdPressed = false;
+            }
+            if ((e.ctrlKey || cmdPressed) && e.keyCode == 82) {
                 // CTRL (CMD) + R reloads the page
                 gui.Window.get().reload();
             }
-            if ((e.ctrlKey || e.metaKey) && e.shiftKey && e.keyCode == 73) {
+            if ((e.ctrlKey || cmdPressed) && e.shiftKey && e.keyCode == 73) {
                 // CTRL (CMD) + SHIFT + I shows devtools window
                 gui.Window.get().showDevTools();
             }
-        }
+        };
+        
+        document.addEventListener('keydown', keyDown, false);
         document.addEventListener('keyup', keyUp, false);
     };
     
