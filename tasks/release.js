0e35e46016cecc30215bdeeaeea8e8a7a96620a4
Jakub Szwacz <jakub@szwacz.com>
2015-04-19 14:33:21 +0200

    Refactored Windows release process

--- tasks/release.js
+++ tasks/release.js
@@ -6,6 +6,7 @@ var utils = require('./utils');
 var releaseForOs = {
     osx: require('./release_osx'),
     linux: require('./release_linux'),
+    windows: require('./release_windows'),
 };
 
 gulp.task('release', ['build'], function () {
---
+++ tasks/release_windows.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var Q = require('q');
+var gulpUtil = require('gulp-util');
+var childProcess = require('child_process');
+var jetpack = require('fs-jetpack');
+var utils = require('./utils');
+
+var projectDir;
+var tmpDir;
+var releasesDir;
+var readyAppDir;
+var manifest;
+
+var init = function () {
+    projectDir = jetpack;
+    tmpDir = projectDir.dir('./tmp');
+    releasesDir = projectDir.dir('./releases');
+    manifest = projectDir.read('app/package.json', 'json');
+    readyAppDir = tmpDir.cwd(manifest.name);
+
+    return Q();
+};
+
+var copyRuntime = function () {
+    return projectDir.copyAsync('node_modules/nw/nwjs', readyAppDir.path(), { overwrite: true });
+};
+
+var copyBuiltApp = function () {
+    return projectDir.copyAsync('build', readyAppDir.path(), { overwrite: true });
+};
+
+var packToDebFile = function () {
+    var deferred = Q.defer();
+
+    var filename = manifest.name + '_' + manifest.version + '.exe';
+    var installScript = projectDir.read('resources/windows/installer.nsi');
+    installScript = utils.replace(installScript, {
+        name: manifest.name,
+        productName: manifest.productName,
+        version: manifest.version,
+        src: readyAppDir.path(),
+        dest: releasesDir.path(filename),
+        icon: projectDir.path('resources/windows/icon.ico'),
+        setupIcon: projectDir.path('resources/windows/setup-icon.ico'),
+        banner: projectDir.path('resources/windows/setup-banner.bmp'),
+    });
+    tmpDir.write('installer.nsi', installScript);
+
+    gulpUtil.log('Building installer with NSIS...');
+
+    // Note: NSIS have to be added to PATH (environment variables).
+    var nsis = childProcess.spawn('makensis', [tmpDir.path('installer.nsi')]);
+    nsis.stdout.pipe(process.stdout);
+    nsis.stderr.pipe(process.stderr);
+    nsis.on('close', function () {
+        gulpUtil.log('Installer ready!', );
+        deferred.resolve();
+    });
+
+    return deferred.promise;
+};
+
+var cleanClutter = function () {
+    return tmpDir.removeAsync('.');
+};
+
+module.exports = function () {
+    return init()
+    .then(copyRuntime)
+    .then(copyBuiltApp)
+    .then(packToDebFile)
+    .then(cleanClutter);
+};
